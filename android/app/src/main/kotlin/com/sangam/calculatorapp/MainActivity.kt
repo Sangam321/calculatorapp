b26985f40ce14af391ebfd206e29b9c6843b9cbc
Sangam Basnet <sangambasnet1800@gmail.com>
2024-12-10 10:40:32 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/sangam/calculatorapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sangam.calculatorapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:calculatorapp/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: CalculatorView()));
+}
